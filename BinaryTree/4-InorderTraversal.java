47c4be1927bc89a26c3bcaa567583b272508c5b2
Shreyas Raj <123464509+shreyasraj-001@users.noreply.github.com>
2025-05-27 13:43:40 +0530

    Update 4-InorderTraversal.java

--- BinaryTree/4-InorderTraversal.java
+++ BinaryTree/4-InorderTraversal.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    private void inorder(TreeNode root, List<Integer> ans){
+        if(root ==null) return;
+        inorder(root.left, ans);
+        ans.add(root.val);
+        inorder(root.right, ans);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+     inorder(root,ans);
+     return ans;  
+        
+    }
+}
