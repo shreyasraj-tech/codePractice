91ea3308810329c571beb425c46469f0fe2735af
Shreyas Raj <123464509+shreyasraj-001@users.noreply.github.com>
2025-05-27 13:39:06 +0530

    Update 226.-InvertBinaryTree.java

--- BinaryTree/226.-InvertBinaryTree.java
+++ BinaryTree/226.-InvertBinaryTree.java
@@ -1,1 +1,17 @@
 
+class Solution {
+    private void invert(TreeNode root){
+        if (root ==null) return ;
+        if(root.left ==null && root.right ==null) return;
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invert(root.left);
+        invert(root.right);
+    }
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+}
